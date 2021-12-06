e78054e187270e997a44dd14ea8e2fb82ce734f3
MQmdm <77150331+MQmdm@users.noreply.github.com>
2021-12-06 09:50:42 +0100

    Update cluster_by_episodes.py

--- cluster_by_episodes.py
+++ cluster_by_episodes.py
@@ -1,12 +1,15 @@
-def make_cluster(x):
-    clusters=[3, 10, 30, 100]
-    for i, j in enumerate (clusters):
-        if x<j: return i
-    return len(clusters)
+
 
 def cluster_by_episodes(df):
+    
+    def _make_cluster(x):
+        clusters=[3, 10, 30, 100]
+        for i, j in enumerate (clusters):
+            if x<j: return i
+        return len(clusters)
+    
     c=df.columns
     df=df.groupby(c[0]).count()
-    df[c[1]]=df[c[1]].apply(make_cluster)
+    df[c[1]]=df[c[1]].apply(_make_cluster)
     df[c[2]]=df[c[2]].apply(lambda x:1)
     return df
